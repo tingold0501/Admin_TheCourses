06e0d7915d5aab2430e7b0a056c4e320fac5e163
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-01-01 15:21:01 +0700

    ....

--- src/pages/dashboard/tables.jsx
+++ src/pages/dashboard/tables.jsx
@@ -15,12 +15,29 @@ import ModalUser from "@/component/ModalUser";
 import { useEffect, useState } from "react";
 import ModalRole from "@/component/ModalRole";
 import Swal from "sweetalert2";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function Tables() {
+  <ToastContainer
+    position="top-right"
+    autoClose={1000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+  />
+  {/* Same as */ }
+  <ToastContainer />
   const urlApi = 'http://127.0.0.1:8000/api/';
   const [roles, setRoles] = useState([]);
   const [users, setUsers] = useState([]);
- 
+
   const deleteRole = (id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -32,11 +49,36 @@ export function Tables() {
       confirmButtonText: "Yes, delete it!"
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "Your file has been deleted." + roles.name,
-          icon: "success"
-        });
+        axios({
+          method: 'post',
+          url: urlApi + 'deleteRole',
+          data: {
+            id: id
+          }
+        }).then((res) => {
+          if (res.data.check == true) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+            window.location.reload();
+          }
+          else if (res.data.check == false) {
+            toast.error('🦄' + res.data.msg,
+              {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+          }
+        })
+
       }
     });
   }
@@ -46,6 +88,14 @@ export function Tables() {
       .then((res) => res.json())
       .then((res) => {
         setRoles(res);
+        console.log(res);
+      });
+  }, []);
+  useEffect(() => {
+    fetch(urlApi + "getData")
+      .then((res) => res.json())
+      .then((res) => {
+        setUsers(res);
         console.log(res);
       });
   }, []);
@@ -135,7 +185,7 @@ export function Tables() {
                           Edit
                         </Typography>
                       </td>
-                      <td  className={className}>
+                      <td className={className}>
                         <Typography
                           onClick={(e) => deleteRole(itemRole.id)}
                           as="a"
@@ -155,6 +205,147 @@ export function Tables() {
         </CardBody>
       </Card>
       <Card>
+        <CardHeader variant="gradient" color="gray" className="mb-8 p-6 flex items-center justify-between">
+          <Typography variant="h6" color="white">
+            Users Table
+          </Typography>
+          <ModalUser/>
+          {/* <Button className="bg-white text-black">Add User</Button> */}
+        </CardHeader>
+        <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+          <table className="w-full min-w-[640px] table-auto">
+            <thead>
+              <tr>
+                {[" name", "email", "role name", "created at", "status", "updated at", "", ""].map((el) => (
+                  <th
+                    key={el}
+                    className="border-b border-blue-gray-50 py-3 px-5 text-left"
+                  >
+                    <Typography
+                      variant="small"
+                      className="text-[11px] font-bold uppercase text-blue-gray-400"
+                    >
+                      {el}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(
+                (itemUsers, key) => {
+                  const className = `py-3 px-5 ${key === roles.length - 1
+                    ? ""
+                    : "border-b border-blue-gray-50"
+                    }`;
+
+                  return (
+                    <tr key={itemUsers.name}>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          {/* <Avatar src={img} alt={name} size="sm" variant="rounded" /> */}
+                          <div>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-semibold"
+                            >
+                              {itemUsers.name}
+                            </Typography>
+                            {/* <Typography className="text-xs font-normal text-blue-gray-500">
+                              {email}
+                            </Typography> */}
+                          </div>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          {/* <Avatar src={img} alt={name} size="sm" variant="rounded" /> */}
+                          <div>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-semibold"
+                            >
+                              {itemUsers.email}
+                            </Typography>
+                            {/* <Typography className="text-xs font-normal text-blue-gray-500">
+                              {email}
+                            </Typography> */}
+                          </div>
+                        </div>
+                      </td>
+                      <td className={className}>
+                        <div className="flex items-center gap-4">
+                          {roles.map((role) => {
+                            // Thêm điều kiện để chỉ hiển thị thông tin của vai trò của người dùng
+                            if (role.id === itemUsers.idRole) {
+                              return (
+                                <div key={role.id}>
+                                  <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-semibold"
+                                  >
+                                    {role.name}
+                                  </Typography>
+                                </div>
+                              );
+                            }
+                            return null; // Tránh trường hợp không thỏa mãn điều kiện
+                          })}
+                        </div>
+                      </td>
+
+                      <td className={className}>
+                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                          {itemUsers.created_at}
+                        </Typography>
+                        {/* <Typography className="text-xs font-normal text-blue-gray-500">
+                          {job[1]}
+                        </Typography> */}
+                      </td>
+                      <td className={className}>
+                        <Chip
+                          variant="gradient"
+                          color={itemUsers.status ? "green" : "blue-gray"}
+                          value={itemUsers.status ? "Opening" : "Closing"}
+                          className="py-0.5 px-2 text-[11px] font-medium w-fit"
+                        />
+                      </td>
+                      <td className={className}>
+                        <Typography className="text-xs font-semibold text-blue-gray-600">
+                          {itemUsers.updated_at}
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          as="a"
+                          href="#"
+                          className="text-xs font-semibold text-blue-gray-600"
+                        >
+                          Edit
+                        </Typography>
+                      </td>
+                      <td className={className}>
+                        <Typography
+                          onClick={(e) => deleteRole(itemUsers.id)}
+                          as="a"
+                          href="#"
+                          className="text-xs font-semibold text-blue-gray-600"
+                        >
+                          Delete
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </CardBody>
+      </Card>
+      {/* <Card>
         <CardHeader variant="gradient" color="gray" className="mb-8 p-6 flex items-center justify-between">
           <Typography variant="h6" color="white">
             User Table
@@ -261,7 +452,7 @@ export function Tables() {
             </tbody>
           </table>
         </CardBody>
-      </Card>
+      </Card> */}
     </div>
   );
 }
