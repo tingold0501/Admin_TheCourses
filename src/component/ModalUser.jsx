5fdf1301a789f5c220eedba3bfe30623a51c693f
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-01-19 15:23:45 +0700

    .....

--- src/component/ModalUser.jsx
+++ src/component/ModalUser.jsx
@@ -81,7 +81,7 @@ function ModalUser() {
                     name: name,
                     email: email,
                     status: 1,
-                    role:role
+                    role: role
                 }
             }).then((res) => {
                 console.log(res);
@@ -99,160 +99,106 @@ function ModalUser() {
     return (
         <div>
             <ToastContainer />
-            <Button variant="outlined" color='success' onClick={handleOpen}>Open Modal</Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        User Modal
-                    </Typography>
-                    <Typography id="modal-modal-description" >
-                        <div className="w-full max-w-lg">
-                            <div className="flex flex-wrap -mx-3 mb-6">
-                                <div className="w-full  px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="grid-first-name"
-                                    >
-                                        Full Name
-                                    </label>
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                        id="grid-first-name"
-                                        type="text"
-                                        placeholder="Nguyen Van A"
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                    {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
-                                </div>
-                                <div className="w-full  px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor=""
-                                    >
-                                        Email
-                                    </label>
-                                    <input
-                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                        id="grid-first-name"
-                                        type="text"
-                                        placeholder="example@gmail.com"
-                                        onChange={(e) => setEmail(e.target.value)}
-                                    />
-                                    {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
-                                </div>
-
+            <>
+                {/* Button trigger modal */}
+                <button
+                    type="button"
+                    className="w-full px-4 py-2 text-sm  font-medium text-black bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                >
+                    Add User
+                </button>
+                {/* Modal */}
+                <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex={-1}
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5 text-black" id="exampleModalLabel">
+                                    Add New User
+                                </h1>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                />
                             </div>
-                            <div className="flex flex-wrap -mx-3 mb-6">
-                                <div className="w-full px-3">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="grid-password"
-                                    >
-                                        Password
-                                    </label>
-                                    <input
-                                        disabled
-                                        className=" appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        id="grid-password"
-                                        type="password"
-                                        placeholder="******************"
-                                    />
-                                    <p className="text-gray-600 text-xs italic">
-                                        Password will auto rang
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="flex flex-wrap -mx-3 mb-2">
-                                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="grid-state"
-                                    >
-                                        Status
-                                    </label>
-                                    <div className="relative">
+                            <div className="modal-body">
+                                <>
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            @
+                                        </span>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="Nguyen Van A"
+                                            aria-label="Name"
+                                            aria-describedby="basic-addon1"
+                                        />
 
-                                        <select
-                                            className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                            id="grid-state"
-                                        >
-                                            <option>Opening</option>
-                                            <option>Cloesing</option>
+                                    </div>
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            #
+                                        </span>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="09127561226"
+                                            aria-label="Phone"
+                                            aria-describedby="basic-addon1"
+                                        />
+
+                                    </div>
+                                    <div className="input-group mb-3">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="NguyenVanA0101@gmail.com"
+                                            aria-label="Recipient's username"
+                                            aria-describedby="basic-addon2"
+                                        />
+                                        <span className="input-group-text" id="basic-addon2">
+                                            @example.com
+                                        </span>
+                                    </div>
+                                    <div className="input-group mb-3">
+                                        <select className='form-control'>
+                                            {roles.map((item) => (
+                                                <option key={item.id} value={item.id}>
+                                                    {item.name}
+                                                </option>
+                                            ))}
                                         </select>
-                                        {/* <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                            <svg
-                                                className="fill-current h-4 w-4"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20"
-                                            >
-                                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                                            </svg>
-                                        </div> */}
                                     </div>
-                                </div>
-                                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                                        htmlFor="grid-state"
-                                    >
-                                        Role
-                                    </label>
-                                    {roles.length > 0 && (
-                                        <div className="relative">
-                                            <select
-                                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                                id="grid-state"
-                                            >
-                                                <option value="" disabled>Select Role</option>
-                                                {roles.map((item) => (
-                                                    <option key={item.id} value={item.id}>
-                                                        {item.name}
-                                                    </option>
-                                                ))}
-                                            </select>
+                                </>
 
-                                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                                <svg
-                                                    className="fill-current h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                >
-                                                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    )}
-                                </div>
-                                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mt-3 mb-2"
-                                        htmlFor="grid-state"
-                                    >
-                                    </label>
-                                    <div className="relative mb-2">
-                                        <LoadingButton
-                                            color="secondary"
-                                            onClick={submitAddUser(roles.name)}
-                                            loading={loading}
-                                            loadingPosition="start"
-                                            startIcon={<SaveIcon />}
-                                            variant="outlined"
-                                        >
-                                            <span>Save</span>
-                                        </LoadingButton>
-
-                                    </div>
-                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary text-black"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Close
+                                </button>
+                                <button type="button" className="btn btn-primary text-black">
+                                    Save changes
+                                </button>
                             </div>
                         </div>
-                    </Typography>
-                </Box>
-            </Modal>
+                    </div>
+                </div>
+            </>
+
         </div>
     )
 }
